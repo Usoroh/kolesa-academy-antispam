c0a3355462cb51f05c981babd30ace6d596e8d13
Usoroh Paius <paius.usoroh@gmail.com>
2023-08-15 19:14:06 +0600

    обрабатываю запрос

--- public/index.php
+++ public/index.php
@@ -1,17 +1,33 @@
 <?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/MessageNormalizer.php';
+require_once __DIR__ . '/../src/SpamDetector.php';
 
-use src\MessageNormalizer;
+use src\SpamDetector;
 
-$normalizer = new MessageNormalizer(__DIR__ . '/../docs/stopwords.txt', __DIR__ . '/../docs/blocklist.txt');
+//Редис
+$redisConfig = [
+    'scheme' => 'tcp',
+    'host'   => 'redis',
+    'port'   => 6379,
+];
 
-$testMessage = "12345, 67890, а также другие числа";
+//создаем инстанс SpamDetector-а
+$spamDetector = new \src\SpamDetector(__DIR__ . '/../docs/stopwords.txt', __DIR__ . '/../docs/blocklist.txt', $redisConfig);
 
-$tokens = $normalizer->normalize($testMessage);
+//Проверяем что был сделан POST запрос с нужными данными (сообщение, айди клиента)
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['message']) && isset($_POST['clientID'])) {
+    $message = $_POST['message'];
+    $clientID = $_POST['clientID'];
+    $checkRate = isset($_POST['check_rate']) && $_POST['check_rate'] == '1';
 
-header('Content-type: application/json; charset=utf-8');
-echo json_encode(['status' => 'ok', 'message' => 'Kek', 'normalized_text' => $tokens]);
+    //проверяем сообщение на спам
+    $result = $spamDetector->checkSpam($message, $clientID, $checkRate);
 
-
-
-
+    header('Content-type: application/json; charset=utf-8');
+    echo json_encode($result);
+} else {
+    header('Content-type: application/json; charset=utf-8');
+    echo json_encode(['status' => 'error', 'message' => 'Неверный запрос']);
+}
