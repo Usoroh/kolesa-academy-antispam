ab52ea61e366aee5452511f2753f3dd359533778
Usoroh Paius <paius.usoroh@gmail.com>
2023-08-15 17:02:18 +0600

    тестирую класс MessageNormalizer

--- public/index.php
+++ public/index.php
@@ -1,3 +1,17 @@
 <?php
+require_once __DIR__ . '/../src/MessageNormalizer.php';
+
+use src\MessageNormalizer;
+
+$normalizer = new MessageNormalizer(__DIR__ . '/../docs/stopwords.txt', __DIR__ . '/../docs/blocklist.txt');
+
+$testMessage = "12345, 67890, а также другие числа";
+
+$tokens = $normalizer->normalize($testMessage);
+
 header('Content-type: application/json; charset=utf-8');
-echo json_encode(['status' => 'ok', 'message' => 'Kolesa Academy!']);
+echo json_encode(['status' => 'ok', 'message' => 'Kek', 'normalized_text' => $tokens]);
+
+
+
+
