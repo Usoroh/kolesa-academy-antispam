46739b00ece48905b1056dbef53063381a878507
Usoroh Paius <paius.usoroh@gmail.com>
2023-08-15 21:20:54 +0600

    добавляю сессии

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
 
+session_start();
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/MessageNormalizer.php';
 require_once __DIR__ . '/../src/SpamDetector.php';
-
-use src\SpamDetector;
 
 //Редис
 $redisConfig = [
@@ -13,19 +13,24 @@ $redisConfig = [
     'port'   => 6379,
 ];
 
-//создаем инстанс SpamDetector-а
+// создаем инстанс SpamDetector-а
 $spamDetector = new \src\SpamDetector(__DIR__ . '/../docs/stopwords.txt', __DIR__ . '/../docs/blocklist.txt', $redisConfig);
 
-//Проверяем что был сделан POST запрос с нужными данными (сообщение, айди клиента)
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['text']) && isset($_POST['clientID'])) {
+// создаем айди сессии чтобы следить за кол-вом отправленных сообщений
+if (!isset($_SESSION['client_session_id'])) {
+    $_SESSION['client_session_id'] = session_create_id();
+}
+
+// Проверяем что был сделан POST запрос с нужными данными
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['text'])) {
     $message = $_POST['text'];
-    $clientID = $_POST['clientID'];
+    $clientID = $_SESSION['client_session_id'];
     $checkRate = isset($_POST['check_rate']) && $_POST['check_rate'] == '1';
 
-    //проверяем сообщение на спам
+    // проверяем сообщение на спам
     $result = $spamDetector->checkSpam($message, $clientID, $checkRate);
 
-    //составляем ответ
+    // составляем ответ
     $response = [
         'status' => 'ok',
         'spam' => $result['is_spam'],
